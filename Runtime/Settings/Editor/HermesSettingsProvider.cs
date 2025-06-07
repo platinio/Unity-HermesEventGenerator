3a4fd5fe84612288bc692d8a32687eab36a3c70e
James Roman <platinio94@gmail.com>
2025-06-07 16:14:55 -0400

    Create hermes settings provider

---
+++ Runtime/Settings/Editor/HermesSettingsProvider.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace ArcaneOnyx.GameEventGenerator
+{
+    public static class HermesSettingsProvider
+    {
+        [SettingsProvider]
+        public static SettingsProvider CreateMyCustomSettingsProvider()
+        {
+            // First parameter is the path in the Settings window.
+            // Second parameter is the scope of this setting: it only appears in the Project Settings window.
+            var provider = new SettingsProvider("Project/Hermes Settings", SettingsScope.Project)
+            {
+                // By default the last token of the path is used as display name if no label is provided.
+                label = "Hermes Settings",
+                // Create the SettingsProvider and initialize its drawing (IMGUI) function in place:
+                guiHandler = (searchContext) =>
+                {
+                    var settings = HermesSettings.GetSerializedSettings();
+                    var argumentsModulePathSP = settings.FindProperty("argumentsModulePath");
+                    var eventsModulePathSP = settings.FindProperty("eventsModulePath");
+                    
+                    EditorGUILayout.BeginHorizontal();
+                    GUI.enabled = false;
+                    EditorGUILayout.PropertyField(argumentsModulePathSP, new GUIContent("Arguments Module Path"));
+                    GUI.enabled = true;
+                    
+                    if (GUILayout.Button("«", GUILayout.Width(50)))
+                    {
+                        string selectFolderPath = EditorUtility.OpenFolderPanel("Select Directory", "", "");
+                        
+                        if (selectFolderPath.StartsWith(Application.dataPath)) 
+                        {
+                            selectFolderPath = "Assets" + selectFolderPath.Substring(Application.dataPath.Length);
+                        }
+
+                        argumentsModulePathSP.stringValue = selectFolderPath;
+                    }
+                    EditorGUILayout.EndHorizontal();
+                    
+                    EditorGUILayout.BeginHorizontal();
+                    GUI.enabled = false;
+                    EditorGUILayout.PropertyField(eventsModulePathSP, new GUIContent("Events Module Path"));
+                    GUI.enabled = true;
+                    if (GUILayout.Button("«", GUILayout.Width(50)))
+                    {
+                        string selectFolderPath = EditorUtility.OpenFolderPanel("Select Directory", "", "");
+                        
+                        if (selectFolderPath.StartsWith(Application.dataPath)) 
+                        {
+                            selectFolderPath = "Assets" + selectFolderPath.Substring(Application.dataPath.Length);
+                        }
+
+                        eventsModulePathSP.stringValue = selectFolderPath;
+                    }
+                    EditorGUILayout.EndHorizontal();
+                    
+                    EditorGUILayout.PropertyField(settings.FindProperty("useAssemblyDefinitions"));
+
+                    settings.ApplyModifiedProperties();
+                    
+                },
+
+                // Populate the search keywords to enable smart search filtering and label highlighting:
+                keywords = new HashSet<string>(new[] { "Hermes", "Events" })
+            };
+
+            return provider;
+        }
+    }
+}
---
+++ Runtime/Settings/HermesSettings.cs
@@ -0,0 +1,55 @@
+#if UNITY_EDITOR
+using System.IO;
+using UnityEditor;
+#endif
+using UnityEngine;
+
+namespace ArcaneOnyx.GameEventGenerator
+{
+    public class HermesSettings : ScriptableObject
+    {
+        [SerializeField] private string argumentsModulePath = "Assets/Hermes.Generated/Arguments";
+        [SerializeField] private string eventsModulePath = "Assets/Hermes.Generated/Events";
+        [SerializeField] private bool useAssemblyDefinitions = false;
+        
+        #if UNITY_EDITOR
+        public static HermesSettings GetOrCreateSettings()
+        {
+            var guids = AssetDatabase.FindAssets($"t:{typeof(HermesSettings).Name}");
+            HermesSettings settings = null;
+            
+            foreach (var guid in guids)
+            {
+                var path = AssetDatabase.GUIDToAssetPath(guid);
+                settings = AssetDatabase.LoadAssetAtPath(path, typeof(HermesSettings)) as HermesSettings;
+                if (settings == null) continue;
+
+                return settings;
+            }
+
+           
+            string directoyPath = "Assets/Editor";
+            string fileName = "HermesSettings.asset";
+
+            if (!Directory.Exists(directoyPath))
+            {
+                Directory.CreateDirectory(directoyPath);
+            }
+            
+            settings = CreateInstance<HermesSettings>();
+            AssetDatabase.CreateAsset(settings, $"{directoyPath}/{fileName}");
+            AssetDatabase.SaveAssets();
+            
+            return settings;
+        }
+        
+        internal static SerializedObject GetSerializedSettings()
+        {
+            return new SerializedObject(GetOrCreateSettings());
+        }
+        #endif
+        
+        
+    }
+}
+
